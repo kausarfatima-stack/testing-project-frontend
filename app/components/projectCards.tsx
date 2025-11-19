ba4f4eb2cef23c67fd3fa84310c399d102ae6caf
kausarfatima-stack <kausar.fatima@codingcops.org>
2025-11-19 13:00:28 +0500

    ...

--- app/components/projectCards.tsx
+++ app/components/projectCards.tsx
@@ -13,7 +13,7 @@ export default function ProjectCards() {
     return <>
         <div className="grid gap-8 xl:grid-cols-3 sm:grid-cols-2 lg:gap-12 p-6 md:p-10 mt-4">
             <div
-                className="flex flex-col transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl lg:p-8 lg:flex-row lg:space-y-0 lg:space-x-6">
+                className="flex flex-col transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl p-6 flex-row space-y-0 space-x-6">
                 <div className="flex-1">
                     <h5 className="mb-3 text-xl font-bold lg:text-2xl">Project Name</h5>
                     <p className="mb-6 text-lg text-gray-600">Here is description for Project</p>
@@ -34,7 +34,7 @@ export default function ProjectCards() {
                 </div>
             </div>
             <div
-                className="flex flex-col transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl lg:p-8 lg:flex-row lg:space-y-0 lg:space-x-6">
+                className="flex flex-col transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl p-6 flex-row space-y-0 space-x-6">
 
                 <div className="flex-1">
                     <h5 className="mb-3 text-xl font-bold lg:text-2xl">Project Name</h5>
@@ -56,7 +56,7 @@ export default function ProjectCards() {
                 </div>
             </div>
             <div
-                className="flex flex-col transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl lg:p-8 lg:flex-row lg:space-y-0 lg:space-x-6">
+                className="flex flex-col transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl p-6 flex-row space-y-0 space-x-6">
                 <div className="flex-1">
                     <h5 className="mb-3 text-xl font-bold lg:text-2xl">Project Name</h5>
                     <p className="mb-6 text-lg text-gray-600">Here is description for Project</p>
